f4a728eb5edc9e520cd751f41c355b9ff3bf5bc4
youceyf11 <Youssef.Ouriniche@emsi-edu.ma>
2025-10-25 18:09:39 +0100

    Initial commit

---
+++ src/main/java/com/devbuild/phdtrackingportal/PhDTrackingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.devbuild.phdtrackingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhDTrackingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhDTrackingPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devbuild/phdtrackingportal/PhDTrackingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbuild.phdtrackingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhDTrackingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
